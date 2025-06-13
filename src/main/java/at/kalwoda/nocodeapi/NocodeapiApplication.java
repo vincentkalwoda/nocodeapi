e6532ed0666d3b828ec7e28c1ac2d746fc6326e4
Vincent Kalwoda <92054282+vincentkalwoda@users.noreply.github.com>
2025-06-13 18:48:19 +0200

    Initial commit

---
+++ src/main/java/at/kalwoda/nocodeapi/NocodeapiApplication.java
@@ -0,0 +1,13 @@
+package at.kalwoda.nocodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocodeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocodeapiApplication.class, args);
+	}
+
+}
